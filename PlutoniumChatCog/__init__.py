9a9601119769e78736fc2174b4e489d5c3a480cb
AusNBNCO <104342386+AusNBNCO@users.noreply.github.com>
2024-08-08 01:30:00 +0800

    Update __init__.py

--- PlutoniumChatCog/__init__.py
+++ PlutoniumChatCog/__init__.py
@@ -1,6 +1,10 @@
 from .mycog import PlutoniumChatCog
 
 async def setup(bot):
-    cog = PlutoniumChatCog(bot)
-    await bot.add_cog(cog)
-    print("Cog setup complete.")
+    try:
+        print("Setting up cog")
+        cog = PlutoniumChatCog(bot)
+        await bot.add_cog(cog)
+        print("Cog setup complete")
+    except Exception as e:
+        print(f"Error in setup: {e}")
